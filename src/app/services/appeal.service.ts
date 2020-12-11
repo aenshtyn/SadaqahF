f4911fd84f0b3dd432079ab4f9479b6446ac7247
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-12-11 12:59:40 +0300

    confirm API intergration

--- src/app/services/appeal.service.ts
+++ src/app/services/appeal.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://127.0.0.1:8000/api/appeals';
+const baseUrl = 'https://sadaqahke.herokuapp.com/api/appeals';
 
 @Injectable({
   providedIn: 'root'
